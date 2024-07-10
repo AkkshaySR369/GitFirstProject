1726def99bffb2a9c887b5ab2b95e209b27a5780
dolly gulhane <dollygulhane@dollys-MacBook-Pro.local>
2024-07-10 22:26:05 +0100

    This is changed now

--- Shellscript/condition.sh
+++ Shellscript/condition.sh
@@ -4,5 +4,5 @@ if [ $1 == "Akshay" ]
 then
     echo "entered value is beginner"
 else
-  echo "entered value is not beginner, it is actually $1"
+  echo "entered value is not beginner, it is actually this: $1"
 fi
